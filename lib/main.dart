d8e0e6047b82b1226cde7a45ca3f8fd4f47f20dd
GAEL_SEBASTIAN_ROJAS_MATA <a.22308051281303@cbtis128.edu.mx>
2025-03-11 16:05:48 +0000

    enviar

--- lib/main.dart
+++ lib/main.dart
@@ -3,40 +3,86 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
-        // useMaterial3: false,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white, // Color de las letras del AppBar
+        ),
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Gael Rojas 1303'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              // Acción al presionar el botón "+"
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: ListView(
+        children: <Widget>[
+          _buildCard("Black Hacker", "https://i.imgur.com/72K720f.png"),
+          _buildCard("White Hacker", "https://i.imgur.com/nJ3uB9M.png"),
+          _buildCard("Black Company", "https://i.imgur.com/Yw1qj9g.png"),
+          _buildCard("Money", "https://i.imgur.com/978Jg3B.png"),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCard(String title, String imageUrl) {
+    return Card(
+      color: Colors.cyan,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minWidth: 180),
+              child: Text(title, style: TextStyle(fontSize: 20)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(imageUrl),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
